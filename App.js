dcc6d2dfe1129461600904e8ce9669dd24a471fe
Efe <knowledgeagare157@gmail.com>
2020-04-05 13:47:22 +0100

    use custom fonts

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { useScreens } from 'react-native-screens';
+
 import { store } from './store/store';
 import ProductNavigation from './navigations/ProductNavigation';
 
+useScreens();
+
+const fetchFonts = async () => {
+  await Promise.all([
+    Font.loadAsync({
+      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    }),
+  ]);
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
   return (
     <Provider store={store}>
       <ProductNavigation />
     </Provider>
   );
 }
-
